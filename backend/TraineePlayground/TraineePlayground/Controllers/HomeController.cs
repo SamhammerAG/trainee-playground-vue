37b702804e1dc8ecc5d492972a4fdbd90ed12c11
schweimer <m.schweimer@oth-aw.de>
2021-07-26 18:46:56 +0200

    Added backend project

---
+++ backend/TraineePlayground/TraineePlayground/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TraineePlayground.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello World";
+        }
+    }
+}
